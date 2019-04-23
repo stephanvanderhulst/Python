1e8e3228b5cab53754fea5d6c39a4f61280cfe00
Stephan van der Hulst <stephanvanderhulst@gmail.com>
2019-04-23 11:03:43 +0200

    added verifyPrime

--- algo.py
+++ algo.py
@@ -13,3 +13,19 @@ def decimalToBinary(decimal):
 
 decimalToBinary(1218)
 print(bin(1218))
+
+#Test a number to see if it is a prime number
+def verifyPrime(candidate):
+    lower_bound = 2
+    upper_bound = int(math.sqrt(candidate))
+    print(f'To determine if {candidate} is a prime number we will divide it by the numbers from 2 to {upper_bound} (floor of the square root of {candidate})')
+    while lower_bound < upper_bound:
+        lower_bound += 1
+        if candidate % lower_bound == 0:
+            print(f'{candidate} is NOT a prime number because it is the multiplication of {lower_bound} and {int(candidate/lower_bound)}')
+            return
+    print(f'{candidate} IS a prime number because it could not be divided by any number from 2 to {upper_bound}')
+
+
+verifyPrime(11233)
+verifyPrime(33216)
